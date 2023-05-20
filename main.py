968ce9e68b3055b9a971c98c76529f57bd92810e
diego-tomaz <54359164+diego-tomaz@users.noreply.github.com>
2023-05-20 19:20:10 -0300

    Separação de conceitos
    Foram adicionadas linhas em branco a cada separação de conceito a fim de melhorar a legibilidade vertical do código.

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ def main():
     while True:
         game = Vinte_e_um()
         jogador_venceu = game.distribui_primeira_rodada()
+        
         print('Embaralhando cartas...')
+        
         if jogador_venceu:
             #CASO O JOGADOR VENÇA A PRIMEIRA RODADA, PERGUNTA SE ELE QUER JOGAR NOVAMENTE.
             resposta = input('Digite S para jogar novamente, ou N para parar de jogar: ').upper()
+            
             if resposta == 'S':
                 continue
             else:
                 sys.exit(1)
         else:
             fim_de_jogo = False
+            
             while not fim_de_jogo:
                 continuar = game.perguntar_jogador_proxima_jogada()
+                
                 if continuar:
                     perdeu = game.inserir_carta_jogador()
                     game.mostrar_score_jogador()
@@ -27,6 +32,7 @@ def main():
                         game.mostrar_score_croupier()
                         
                         resposta = input('Digite S para jogar novamente, ou N para parar de jogar: ').upper()
+                        
                         if resposta == 'S':
                             fim_de_jogo = True
                         else:
@@ -36,7 +42,9 @@ def main():
                     game.mostrar_score_jogador()
                     game.croupier_comprar_cartas_ate_passar_jogador()
                     game.definir_vencedor()
+                    
                     resposta = input('Digite S para jogar novamente, ou N para parar de jogar: ').upper()
+                    
                     if resposta == 'S':
                         fim_de_jogo = True
                     else:
