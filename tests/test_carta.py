0e417bbe9893279f2953506c00ee47e52f4e824e
RodrigoflBraz <rodrigoflbraz@gmail.com>
2023-05-29 15:54:01 -0300

    refatorando testes

--- tests/test_carta.py
+++ tests/test_carta.py
@@ -1,12 +1,13 @@
 import pytest
 from carta import Carta
 
+
 def test_carta_eh_uma_instancia_de_Carta():
     """ Testa se a carta é uma instância de Carta. """
-    carta = Carta('A', '♥')
+    carta = Carta('2', '♥')
     assert isinstance(carta, Carta)
 
-def test_string_de_carta_eh_igual_a_A_de_copas():
+def test_representacao_em_str_da_carta():
     """ Testa se a string da carta é igual a 'A de ♥'. """
     carta = Carta('A', '♥')
     assert str(carta) == 'A♥'
