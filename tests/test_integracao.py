7fbe9b627982c7f5b5892fca664ddeb3e681b068
RodrigoFlBraz <rodrigoflbraz@gmail.com>
2023-06-15 01:56:23 -0300

    escrevendo mais testes

--- tests/test_integracao.py
+++ tests/test_integracao.py
@@ -22,7 +22,6 @@ def crupier():
     return crupier
 
 
-
 def teste_criar_baralho_e_distribuir_primeiras_cartas(baralho, jogador, crupier):
        
         carta1 = baralho.virar()
@@ -45,3 +44,51 @@ def teste_criar_baralho_e_distribuir_primeiras_cartas(baralho, jogador, crupier)
 
         assert str(cartas_Crupier[0]) == 'K♠'
         assert str(cartas_Crupier[1]) == 'K♦'
+
+def teste_score_do_jogador_estoura_apos_comprar_cartas_que_passam_de_21(baralho, jogador, crupier):
+       
+        carta1 = baralho.virar()
+        carta2 = baralho.virar()
+
+        jogador.insert_card(carta1)
+        jogador.insert_card(carta2)
+
+        carta3 = baralho.virar()
+        carta4 = baralho.virar()
+
+        crupier.insert_card(carta3)
+        crupier.insert_card(carta4)
+        
+        carta5 = baralho.virar()
+        jogador.insert_card(carta5)
+
+        score_jogador = jogador.get_score()
+
+        assert score_jogador > 21
+        assert jogador.mao.verifica_se_perdeu() == True
+
+def teste_score_do_jogador_apos_comprar_4_cartas_que_valem_10(baralho, jogador, crupier):
+        carta1 = baralho.virar()
+        carta2 = baralho.virar()
+
+        jogador.insert_card(carta1)
+        jogador.insert_card(carta2)
+
+        carta3 = baralho.virar()
+        carta4 = baralho.virar()
+
+        crupier.insert_card(carta3)
+        crupier.insert_card(carta4)
+        
+        carta5 = baralho.virar()
+        carta6 = baralho.virar()
+        
+        jogador.insert_card(carta5)
+        jogador.insert_card(carta6)
+
+        score_jogador = jogador.get_score()   
+
+    
+        assert score_jogador == 40
+        assert jogador.mao.verifica_se_perdeu() == True
+
