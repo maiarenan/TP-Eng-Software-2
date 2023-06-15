7e171695c1885e7c9411779b1c495824872a5fbb
RodrigoFlBraz <rodrigoflbraz@gmail.com>
2023-06-15 00:03:19 -0300

    Melhorando teste de sistema

--- tests/test_sistema.py
+++ tests/test_sistema.py
@@ -18,15 +18,20 @@ def test_cadastro_jogador_ao_iniciar_jogo():
     assert "Olá Rodrigo, bem vindo ao nosso jogo de 21. Esperamos que você se divirta muito!!!" in output
 
 @pytest.mark.cmd
-def test_score_do_jogador_maior_que_0_apos_comecar_jogo():
+def test_score_do_jogador_maior_que_0_apos_primeira_jogada():
     path =  r"C:\Users\rodri\Desktop\Atividades\app_21\TP-Eng-Software-2\main.py"
     
     processo = subprocess.Popen(['python', path], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     processo.stdin.write('Rodrigo' + "\n")
+    processo.stdin.write('S' + "\n")
+
     
     output, errors = processo.communicate()
     processo.stdin.close()
     
-    score_jogador = int(output.split("Score do jogador: ")[0])
+    score_jogador = int(output.split("Score do jogador: ")[1])
 
     assert score_jogador > 0
+
+@pytest.mark.cmd
+def test_score_do_jogador_maior_que_0_apos_primeira_jogada():
