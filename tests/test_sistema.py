901085aee672bb1e58be0c3513afc5c4d0b5cd23
maiarenan <renanmaiabh@gmail.com>
2023-06-17 11:09:42 -0300

    fix path e ignora naipes para testes

--- tests/test_sistema.py
+++ tests/test_sistema.py
@@ -1,26 +1,58 @@
 import pytest
 import subprocess
 import warnings
-
+import os
+import re
 warnings.filterwarnings("ignore")
 
-@pytest.mark.cmd
-def test_cadastro_jogador_ao_iniciar_jogo():
-    path =  r"C:\Users\rodrigo.braz\Rodrigo_Si\tp_es2\TP-Eng-Software-2\main.py"
-    
-    processo = subprocess.Popen(['python', path], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    processo.stdin.write('Rodrigo' + "\n")
+#prepara o path para o arquivo main.py
+@pytest.fixture
+def path():
+    current_dir = os.path.abspath(os.path.dirname(__file__))
 
-    output, errors = processo.communicate()
-    processo.stdin.close()
+    parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+
+    path_main = os.path.join(parent_dir, "main.py")
     
-    assert "Olá Rodrigo, bem vindo ao nosso jogo de 21. Esperamos que você se divirta muito!!!" in output
+    return path_main
+
+
+@pytest.mark.cmd
+def test_cadastro_jogador_ao_iniciar_jogo(path):
+    warnings.filterwarnings("ignore")
+
+    processo = subprocess.Popen(['python', path], 
+                                stdin=subprocess.PIPE, 
+                                stdout=subprocess.PIPE, 
+                                stderr=subprocess.PIPE, 
+                                universal_newlines=True)
+    
+    naipes = {
+    'HEART SUIT': '\u2665',
+    'DIAMOND SUIT': '\u2666',
+    'CLUB SUIT': '\u2663',
+    'SPADE SUIT': '\u2660'
+    } 
+
+    nome  = "Rodrigo"
+    processo.stdin.write(nome + "\n")
+    processo.stderr.close()
+    processo.stdin.close()
+
+    saida, _ = processo.communicate()
+
+    #monta a regex para remover os naipes
+    regex = re.compile('|'.join(re.escape(naipes) for naipes in naipes.values()))
+
+    saida_limpa = regex.sub('',saida)
+    
+    assert f"Olá {nome}, bem vindo ao nosso jogo de 21. Esperamos que você se divirta muito!!!" in saida_limpa
 
 @pytest.mark.flaky(reruns=5)
 @pytest.mark.cmd
-def test_score_do_jogador_maior_que_0_apos_primeira_jogada():
-    path =  r"C:\Users\rodrigo.braz\Rodrigo_Si\tp_es2\TP-Eng-Software-2\main.py"
-    
+def test_score_do_jogador_maior_que_0_apos_primeira_jogada(path):
+    warnings.filterwarnings("ignore")
+
     processo = subprocess.Popen(['python', path], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     processo.stdin.write('Rodrigo' + "\n")
     processo.stdin.write('S' + "\n")
